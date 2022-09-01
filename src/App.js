3def3d47a491abe242b06db47f516fddb32fde5e
비엔나 <sure@paran.com>
2022-09-01 21:11:14 +0900

    update login logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { login } from "./redux/actions";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 function App() {
-  const navigate = useNavigate();
-  //const [sign, setSign] = useState(false);
-  const { sign } = useSelector((state) => ({
-    sign: state.isLogged,
-  }));
-
-  useEffect(() => {
-    console.log("app.js= " + sign);
-    if (sign === false) navigate("/login");
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Header, Content } from "./../layout/index";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Main = () => {
+  const navigate = useNavigate();
+  const { sign } = useSelector((state) => ({
+    sign: state.isLogged,
+  }));
+
+  useEffect(() => {
+    console.log("main.js= " + sign);
+
+    // 로그인 상태가 아닐 경우 /login으로 이동
+    if (sign === false) navigate("/login");
+  }, []);
   return (
     <div>
       <Header></Header>
