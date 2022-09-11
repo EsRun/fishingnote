f92ec4db1435d1942e0f2f9c2324f706d6c782ff
비엔나 <sure@paran.com>
2022-09-11 20:13:21 +0900

    code cleanup

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -11,43 +11,6 @@ const Content_box = styled.div`
 `;
 
 const Content = () => {
-  const [inputs, setInputs] = useState({
-    aname: "",
-    nickname: "",
-    tests: "",
-    pl1: "",
-    pl2: "",
-    s1: "",
-    s2: "",
-    style: { backgroundColor: "blue" },
-  });
-
-  const { aname, nickname, pl1, pl2, s1, s2, tests, style } = inputs;
-
-  const onChange = (e) => {
-    const { value, name, tests, dataset, placeholder, style } = e.target;
-    console.log(e.target.tests);
-    console.log(e.target);
-    console.log("dataset.style= " + dataset.style);
-    setInputs({
-      ...inputs,
-      [name]: value, // [aname]: input value
-      [dataset.name]: value,
-      [dataset.style]: 50,
-      //[placeholder]: value,
-    });
-  };
-
-  const onReset = (e) => {
-    setInputs({
-      aname: "",
-      nickname: "",
-      tests: "",
-      placeholder: "",
-      style: { backgroundColor: "blue" },
-    });
-  };
-
   const { isLogged } = useSelector((state) => ({
     isLogged: state.isLogged,
   }));
@@ -63,8 +26,6 @@ const Content = () => {
       <Container fluid="xl">
         <Row className="row_right">
           <div className="mb-4 w-25 row_padding_0">
-            <button onClick={add}>+</button>
-            <button onClick={() => dispatch(removeSubscriber())}>-</button>
             <Form.Select aria-label="Default select example">
               <option value="1">최신순</option>
               <option value="2">거리순</option>
@@ -74,35 +35,6 @@ const Content = () => {
         </Row>
         <Row>
           <Card></Card>
-        </Row>
-        <Row>
-          <div>Second Row</div>
-          <input
-            name="aname"
-            data-name="pl1"
-            data-style="s1"
-            placeholder={pl1}
-            onChange={onChange}
-            value={aname}
-            style={{ fontSize: s1 }}
-          />
-          <input
-            name="nickname"
-            data-name="pl2"
-            data-style="s2"
-            placeholder={pl2}
-            onChange={onChange}
-            value={nickname}
-            style={{ fontSize: s2 }}
-          />
-          <button onClick={onReset}>초기화</button>
-          <div>
-            <b>값: </b>
-            {aname} ({nickname})<br></br>
-            <b>pl1: {pl1}</b>
-            <br></br>
-            <b>pl2: {pl2}</b>
-          </div>
         </Row>
       </Container>
     </Content_box>
