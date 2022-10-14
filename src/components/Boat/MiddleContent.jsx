f1faae8d8a89ad27c41463af4c2d2bf78cc1a029
비엔나 <sure@paran.com>
2022-10-14 15:57:24 +0900

    add : useMemo, useCallback

--- src/components/Boat/MiddleContent.jsx
+++ src/components/Boat/MiddleContent.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+import { useCallback } from "react";
+import { useMemo } from "react";
 import { useEffect } from "react";
 import { MiddleBox, Ul, Li } from "../style/BoatStyle";
 
 export default function MiddleContent(props) {
-  useEffect(() => {
-    console.log("pr", props.Nav);
-  });
-  const contentData = [
+  const [select, setSelect] = useState([]);
+
+  const boatArray = [
     { month: "1", species: ["종류1", "종류2", "종류3"], scale: "1, 2, 3" },
     { month: "2", species: ["종류1", "종류2", "종류3"], scale: "3, 4, 5" },
     { month: "3", species: ["종류1", "종류2", "종류3"], scale: "6, 7, 8" },
@@ -28,6 +30,39 @@ export default function MiddleContent(props) {
     },
   ];
 
+  const landArray = [
+    { month: "1", species: ["Land1", "Land2", "Land3"], scale: "1, 2, 3" },
+    { month: "2", species: ["Land1", "Land2", "Land3"], scale: "3, 4, 5" },
+    { month: "3", species: ["Land1", "Land2", "Land3"], scale: "6, 7, 8" },
+    {
+      month: "4",
+      species: ["Land1", "Land2", "Land3"],
+      scale: "10, 11, 12, 13",
+    },
+    { month: "5", species: ["Land1", "Land2", "Land3"], scale: "1, 2" },
+    { month: "6", species: ["Land1", "Land2", "Land3"], scale: "13, 14, 15" },
+    { month: "7", species: ["Land1", "Land2", "Land3"], scale: "4, 5, 6" },
+    { month: "8", species: ["Land1", "Land2", "Land3"], scale: "3, 5, 7" },
+    { month: "9", species: ["Land1", "Land2", "Land3"], scale: "8, 9, 10" },
+    { month: "10", species: ["Land1", "Land2", "Land3"], scale: "3, 4" },
+    { month: "11", species: ["Land1", "Land2", "Land3"], scale: "1, 2" },
+    {
+      month: "12",
+      species: ["Land1", "Land2", "Land3"],
+      scale: "12, 13, 14, 15",
+    },
+  ];
+  /*
+  const setFunc = useCallback(() => {
+    props.Nav === 1 ? setSelect(boatArray) : setSelect(landArray);
+  }, [props.Nav, boatArray, landArray]);
+
+  const sets = useMemo(() => setFunc(props.Nav), [setFunc, props.Nav]);
+*/
+  useEffect(() => {
+    console.log("pr", props.Nav);
+  }, [props.Nav]);
+
   const selectMonth = (e) => {
     e.currentTarget.style.border = "1px solid #ff0000";
   };
@@ -36,7 +71,7 @@ export default function MiddleContent(props) {
     <>
       <MiddleBox>
         <Ul>
-          {contentData.map((value, index) => (
+          {select.map((value, index) => (
             <Li key={index} onClick={selectMonth}>
               <div
                 style={{
