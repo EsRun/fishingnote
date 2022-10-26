118b8db306d757dc54e81f86f260e987a6c08171
비엔나 <sure@paran.com>
2022-10-26 14:17:40 +0900

    ...

--- src/components/Boat/Nav.jsx
+++ src/components/Boat/Nav.jsx
@@ -14,12 +14,6 @@ export default function Nav(props) {
 
   const onToggle = (e) => {};
 
-  /**
-   nav 부분 div로 변경 예정
-   굳이 Link 사용 할 필요가 없을 듯
-   div span으로 처리
-  */
-
   return (
     <>
       <div
@@ -58,31 +52,6 @@ export default function Nav(props) {
           </NavItem>
         </div>
       </div>
-      {/*     
-      <NavUl>
-        <NavLi
-          onClick={() => {
-            props.setNav(1);
-          }}
-        >
-          <Link to="#">선상</Link>
-        </NavLi>
-        <NavLi
-          onClick={() => {
-            props.setNav(2);
-          }}
-        >
-          <Link to="#">도보</Link>
-        </NavLi>
-        <NavLi
-          onClick={() => {
-            props.setNav(3);
-          }}
-        >
-          <Link to="#">기타</Link>
-        </NavLi>
-      </NavUl> */}
-
       <Select onChange={onSelect}>
         {locations.map((option) => (
           <option key={option.value} value={option.value}>
