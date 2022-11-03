78a99ad8a64b9177d773f64bc42861fcac1d4d7c
비엔나 <sure@paran.com>
2022-11-03 11:27:04 +0900

    add : index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,3 +1,44 @@
+import { Container, Row } from "../components/style/Common";
+
 export default function Index() {
-  return <div>Index 페이지</div>;
+  return (
+    <Container>
+      <div style={{ display: "flex", flexFlow: "column" }}>
+        <Row style={{ textAlign: "-webkit-right", marginBottom: "40px" }}>
+          <div
+            style={{
+              padding: "20px",
+              width: "80%",
+              height: "500px",
+              border: "1px solid skyblue",
+            }}
+          >
+            <div
+              style={{
+                height: "100%",
+                backgroundColor: "#abedab",
+              }}
+            ></div>
+          </div>
+        </Row>
+        <Row>
+          <div
+            style={{
+              width: "80%",
+              padding: "20px",
+              height: "500px",
+              border: "1px solid skyblue",
+            }}
+          >
+            <div
+              style={{
+                height: "100%",
+                backgroundColor: "#abeaed",
+              }}
+            ></div>
+          </div>
+        </Row>
+      </div>
+    </Container>
+  );
 }
