a577d6c73c276af00810d360e47c09bd00172738
비엔나 <sure@paran.com>
2022-11-14 08:59:14 +0900

    fix : header style

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -48,13 +48,8 @@ export default function Header() {
                   메뉴3
                 </NavLink>
               </Li>
-              <Li>
-                <NavLink
-                  to="/articles/3"
-                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                >
-                  Logout
-                </NavLink>
+              <Li style={{ padding: "10px 40px" }}>
+                <NavLink to="/logout">Logout</NavLink>
               </Li>
             </Ul>
           </NavBox>
