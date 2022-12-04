9f692ef0f74390a4906f7ad6de37019c1bdfb115
비엔나 <sure@paran.com>
2022-12-04 18:55:49 +0900

    ...

--- src/components/Boat/Nav.jsx
+++ src/components/Boat/Nav.jsx
@@ -28,7 +28,6 @@ export default function Nav(props) {
   const setMethods = (val, idx, e) => {
     setActiveClass(idx);
     props.setNav(val);
-    console.log(e);
   };
 
   const setSelect = (e) => {
