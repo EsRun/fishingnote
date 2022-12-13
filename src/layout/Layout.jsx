ea33d3e2988b81990b9818576b09b63af3946d76
비엔나 <sure@paran.com>
2022-12-13 09:27:15 +0900

    ...

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,10 +3,6 @@ import { Main } from "../components/style/Common";
 import { Header } from "./index";
 
 export default function Layout() {
-  // 인덱스 페이지 기획 고민...
-  // 이달의 어종? 뭐 이런거?
-  // 렌더링 페이지로 가자
-
   return (
     <>
       <Header />
