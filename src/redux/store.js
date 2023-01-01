cecf71da6c2889290d2403523d7ee83b19c03546
비엔나 <sure@paran.com>
2023-01-01 20:17:56 +0900

    ... createStore memo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
 import { createStore } from "redux";
 import subscribersReducer from "./reducer";
+/*
+createStore 
+If you want to use createStore without this visual deprecation warning, use the legacy_createStore import instead:
+
+import { legacy_createStore as createStore} from 'redux'
+*/
 
 const store = createStore(subscribersReducer);
 
